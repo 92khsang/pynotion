f7c362ac0694ca76335ba07c6c7f2002ce9a23db
Hayes <92khsang@gmail.com>
2025-04-02 12:15:08 +0900

    Refactor database model
    Replaced direct imports with forward references and type checking guards
    to prevent circular dependencies. Added docstrings for clarity and
    refined type annotations for database properties, title, and relations.

--- pynotion/models/database.py
+++ pynotion/models/database.py
@@ -1,31 +1,56 @@
-from typing import Literal, Optional, Annotated
+from typing import Literal, Optional, Annotated, TYPE_CHECKING, Union
 
 from pydantic import Field, BeforeValidator
 
+from pynotion.models.object import NotionObjectType, NotionObjectId
 from ._internal import FrozenNotionModel, BaseNotionModel
-from .emoji import Emoji
-from .file import File
-from .object import NotionObjectType, NotionObjectId
-from .parent import PageParent
-from .properties import Property, TxPropertySchema
-from .rich_text import RxRichText, TxRichText
-from .types import NotionDatetime
-from .user import UserRef
+
+if TYPE_CHECKING:
+    from pynotion.models.parent import PageParent
+    from pynotion.models.property.rx import Property
+    from pynotion.models.property.tx import PropertySchema
+    from pynotion.models.types import NotionDatetime
+    from pynotion.models.user import UserRef
+    from pynotion.models.rich_text.rx import RxRichText
+    from pynotion.models.rich_text.tx import TxRichText
+
+__all__ = ["RxDatabase", "CreateDatabase", "UpdateDatabase"]
 
 
 class RxDatabase(FrozenNotionModel):
+    """Represents a database in Notion.
+
+    Attributes:
+        id: The ID of the database.
+        created_time: The date and time when the database was created.
+        created_by: The user who created the database.
+        last_edited_time: The date and time when the database was last edited.
+        last_edited_by: The user who last edited the database.
+        title: The title of the database.
+        description: The description of the database.
+        icon: The icon of the database.
+        cover: The cover of the database.
+        properties: The properties of the database.
+        parent: The parent of the database.
+        url: The URL of the database.
+        archived: Whether the database is archived.
+        in_trash: Whether the database is in the trash.
+        is_inline: Whether the database is inline.
+        public_url: The public URL of the database.
+    """
+
     object: Literal[NotionObjectType.DATABASE] = NotionObjectType.DATABASE
     id: NotionObjectId
-    created_time: Optional[NotionDatetime] = None
-    created_by: Optional[UserRef] = None
-    last_edited_time: Optional[NotionDatetime] = None
-    last_edited_by: Optional[UserRef] = None
-    title: Optional[list[RxRichText]] = None
-    description: Optional[list[RxRichText]] = None
-    icon: Optional[File | Emoji] = None
-    cover: Optional[File] = None
-    properties: Optional[dict[str, Property]] = None
-    parent: Optional[PageParent] = None
+    created_time: Optional["NotionDatetime"] = None
+    created_by: Optional["UserRef"] = None
+    last_edited_time: Optional["NotionDatetime"] = None
+    last_edited_by: Optional["UserRef"] = None
+    title: Optional[list["RxRichText"]] = None
+    description: Optional[list["RxRichText"]] = None
+    icon: Union["None | NotionFile | NotionEmoji"] = None
+    cover: Union["None | NotionFile"] = None
+    properties: Optional[dict[str, "Property"]] = None
+    parent: Optional["PageParent"] = None
     url: Optional[str] = Field(default=None, frozen=True)
     archived: Optional[bool] = None
     in_trash: Optional[bool] = None
@@ -34,18 +59,31 @@ class RxDatabase(FrozenNotionModel):
 
 
 class CreateDatabase(BaseNotionModel):
+    """Model for creating a database.
+
+    Attributes:
+        parent: The parent of the database.
+        title: The title of the database.
+        properties: The properties of the database.
+    """
+
     parent: Annotated[
-        str | PageParent,
+        "str | PageParent",
         BeforeValidator(lambda v: PageParent(page_id=v) if isinstance(v, str) else v),
     ]
-    title: Optional[list[TxRichText]]
-    properties: dict[str, TxPropertySchema]
+    title: Optional[list["TxRichText"]]
+    properties: dict[str, "PropertySchema"]
 
 
 class UpdateDatabase(BaseNotionModel):
-    title: Optional[list[TxRichText]]
-    description: Optional[list[TxRichText]]
-    properties: dict[str, TxPropertySchema]
+    """Model for updating a database.
 
+    Attributes:
+        title: The title of the database.
+        description: The description of the database.
+        properties: The properties of the database.
+    """
 
-TxDatabase = CreateDatabase | UpdateDatabase
+    title: Optional[list["TxRichText"]]
+    description: Optional[list["TxRichText"]]
+    properties: dict[str, Optional["PropertySchema"]]
